dfb06f42769884c8cf98e367f2700fbe00db6a6d
Courtney Zavaleta <courtneyzavaleta@gmail.com>
2020-04-27 04:10:07 +0100

    alksdfj

--- server.js
+++ server.js
@@ -27,12 +27,12 @@ db.on('open', () => {
 });
 
 //Enable CORS for all HTTP methods
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
 
 //SHOW
